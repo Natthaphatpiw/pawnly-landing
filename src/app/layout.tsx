75ea2693f31ae826ef1bfee62488ca210855fb71
Piw <piw@Piws-MacBook-Air.local>
2026-01-03 17:13:25 +0700

    แก้ไข

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Sarabun } from "@next/font/google";
+import { Sarabun } from "next/font/google";
 import "./globals.css";
 
 const sarabun = Sarabun({
